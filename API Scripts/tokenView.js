d5b6883ccd5d5c8068f0efc599938509bf35dc35
TheScottishninja <46663739+TheScottishninja@users.noreply.github.com>
2021-04-03 16:41:58 -0400

    flip token base on facing rotation

--- API Scripts/tokenView.js
+++ API Scripts/tokenView.js
@@ -1,13 +1,26 @@
 
 on("change:graphic", _.debounce((obj,prev)=>{
 	log("view change")
-    if(obj.get('left')==prev['left'] && obj.get('top')==prev['top']) return;
+    if(obj.get('left')==prev['left'] && obj.get('top')==prev['top'] && obj.get('rotation')==prev['rotation']) return;
 
     if(obj.get("name").includes("_facing")){
     	//position must match original token
     	token = getObj("graphic", obj.get("name").substring(0, obj.get("name").indexOf("_")));
     	obj.set("left", token.get("left"))
     	obj.set("top", token.get("top"))
+
+    	//change direction of token to match facing
+    	//assume left facing to start
+
+    	// log(obj.get("rotation"))
+    	if((obj.get("rotation") % 360) < 180){
+    		//face right
+    		token.set("fliph", true)
+    	}
+    	else{
+    		//face left
+    		token.set("fliph", false)
+    	}
     }
     else {
     	var facing = findObjs({
@@ -23,3 +36,17 @@ on("change:graphic", _.debounce((obj,prev)=>{
     }
 
 }));
+
+on("destroy:graphic", function(obj){
+    log('remove facing')
+
+    var facing = findObjs({
+        _type: "graphic",
+        _pageid: obj.get("pageid"),
+        name: obj.get("id") + "_facing",
+    })[0];
+
+    if(facing){
+    	facing.remove()
+    }
+});
